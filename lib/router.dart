130bea34e79366378ed4b0c6e9d0649d53244650
navgarg <navgarg06@gmail.com>
2025-07-16 12:21:53 +0530

    chore: remove unnecessary code

--- lib/router.dart
+++ lib/router.dart
@@ -1,14 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rideshare/providers/auth/auth_provider.dart';
 import 'package:rideshare/screens/home_page.dart';
 import 'package:rideshare/screens/sign_in_page.dart';
 
-// final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
 final router = GoRouter(
-  // navigatorKey: navigatorKey,
   redirect: (context, state) {
     final getIt = GetIt.instance;
 
