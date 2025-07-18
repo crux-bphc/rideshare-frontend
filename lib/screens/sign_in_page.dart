e853b63a1fe65e31278f4f36a5177126f2c1a77f
navgarg <navgarg06@gmail.com>
2025-07-18 13:02:23 +0530

    feat: update sign-in screen ui

--- lib/screens/sign_in_page.dart
+++ lib/screens/sign_in_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:rideshare/widgets/custom_elevated_button.dart';
 import 'package:signals_flutter/signals_flutter.dart';
 
 import '../providers/auth/auth_provider.dart';
@@ -9,21 +10,24 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color(0xFF1E2128),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            Image.asset(
+              'assets/logo.png',
+              height: 150,
+            ),
+            // const SizedBox(height: 24),
             Text(
-              "RideShare",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 48,
+              'RideShare',
+              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 50),
+            SizedBox(height: 16),
             _LoginWidget(),
           ],
         ),
@@ -46,18 +50,16 @@ class _LoginWidget extends StatelessWidget {
           duration: const Duration(milliseconds: 300),
           child: isLoading
               ? const CircularProgressIndicator(color: Colors.white)
-              : OutlinedButton.icon(
-            icon: const Icon(Icons.login_rounded),
-            onPressed: () {
-              GetIt.instance<AuthProvider>().login();
-            },
-            label: const Text('Sign in with Google'),
-            style: OutlinedButton.styleFrom(
-              foregroundColor: const Color(0xFFEBCB8B),
-              side: const BorderSide(color: Color(0xFFEBCB8B)),
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-            ),
-          ),
+              : CustomElevatedButton(
+                  onPressed: () {
+                    GetIt.instance<AuthProvider>().login();
+                  },
+                  text: 'Sign in with Google',
+                  icon: const Icon(
+                    Icons.login_rounded,
+                    color: Colors.white,
+                  ),
+                ),
         );
       },
     );
