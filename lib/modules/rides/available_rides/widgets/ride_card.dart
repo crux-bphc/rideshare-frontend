abec2995525dbfe8ca9224cb5f05431abe7b1271
navgarg <navgarg06@gmail.com>
2025-12-29 19:08:03 +0530

    fix: update imports

--- lib/modules/rides/available_rides/widgets/ride_card.dart
+++ lib/modules/rides/available_rides/widgets/ride_card.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rideshare/models/ride.dart';
 import 'package:rideshare/providers/auth/auth_provider.dart';
-import 'package:rideshare/providers/rides/bookmarks_controller.dart';
+import 'package:rideshare/shared/providers/rides_provider.dart';
 import 'package:intl/intl.dart';
 import 'package:rideshare/shared/theme.dart';
 
